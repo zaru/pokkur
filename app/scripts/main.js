3d60d14ddc0d4de1c291e59defbb276c35b901db
zaru@sakuraba <zarutofu@gmail.com>
2014-10-19 14:20:58 +0900

    add DropboxAPI

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,3 +9,20 @@ var content = new Vue({
   data: {
   }
 });
+
+
+var client = new Dropbox.Client({key: 'ox8biz2img8hvtr'});
+
+// Try to finish OAuth authorization.
+client.authenticate({interactive: false}, function (error) {
+    if (error) {
+        alert('Authentication error: ' + error);
+    }
+});
+
+if (client.isAuthenticated()) {
+    // Client is authenticated. Display UI.
+    console.log('auth');
+}
+
+client.authenticate();
