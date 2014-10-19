b697d67bb74cda464269c9c6feecc0e1c2fa6b68
zaru@sakuraba <zarutofu@gmail.com>
2014-10-19 22:44:25 +0900

    タブでインデントするように実装

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,35 @@
 var vue = new Vue({
     el: '#content',
     data: {
-        message: "```php\n$test = 'piyo';\n```\n# hoge\ntest.test.test."
+        message: "```php\n$test = 'piyo';\n```\n# hoge\ntest. test. test.\n- list\n- list\n- list\n"
     },
     filters: {
         marked: marked
+    },
+    methods: {
+      insert: function(e) {
+        // タブ：専用のライブラリ入れたほうがいいかも…
+        if (e.keyCode === 9) {
+          if (e.shiftKey === false) {
+            e.preventDefault();
+            var elem = e.target;
+            var val = elem.value;
+            var pos = elem.selectionStart;
+            elem.value = val.substr(0, pos) + '\t' + val.substr(pos, val.length);
+            elem.setSelectionRange(pos + 1, pos + 1);
+          } else {
+            e.preventDefault();
+            var elem = e.target;
+            var val = elem.value;
+            var pos = elem.selectionStart;
+            var firstPart = val.substr(0, pos).split("\n");
+            var cursorLine = firstPart.pop().replace(/^\t/, '');
+            firstPart = firstPart.join("\n") + "\n" + cursorLine;
+            elem.value = firstPart + val.substr(pos, val.length);
+            elem.setSelectionRange(pos - 1, pos - 1);
+          }
+        }
+      }
     }
 });
 
