99faa254f8a297aa35ab8cea5945174512bdfbf1
zaru@sakuraba <zarutofu@gmail.com>
2014-10-19 23:29:59 +0900

    ドラッグ可能にした

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,34 +1,61 @@
 var vue = new Vue({
-    el: '#content',
-    data: {
-        message: "```php\n$test = 'piyo';\n```\n# hoge\ntest. test. test.\n- list\n- list\n- list\n"
+  el: '#content',
+  data: {
+      message: "```php\n$test = 'piyo';\n```\n# hoge\ntest. test. test.\n- list\n- list\n- list\n"
+  },
+  filters: {
+      marked: marked
+  },
+  methods: {
+    insert: function(e) {
+      // タブ：専用のライブラリ入れたほうがいいかも…
+      if (e.keyCode === 9) {
+        if (e.shiftKey === false) {
+          e.preventDefault();
+          var elem = e.target;
+          var val = elem.value;
+          var pos = elem.selectionStart;
+          elem.value = val.substr(0, pos) + '\t' + val.substr(pos, val.length);
+          elem.setSelectionRange(pos + 1, pos + 1);
+        } else {
+          e.preventDefault();
+          var elem = e.target;
+          var val = elem.value;
+          var pos = elem.selectionStart;
+          var firstPart = val.substr(0, pos).split("\n");
+          var cursorLine = firstPart.pop().replace(/^\t/, '');
+          firstPart = firstPart.join("\n") + "\n" + cursorLine;
+          elem.value = firstPart + val.substr(pos, val.length);
+          elem.setSelectionRange(pos - 1, pos - 1);
+        }
+      }
     },
-    filters: {
-        marked: marked
+  }
+});
+
+var separate = new Vue({
+    el: '#separate',
+    p: {
+      separateX: 0,
+      previewWidth: 0
     },
     methods: {
-      insert: function(e) {
-        // タブ：専用のライブラリ入れたほうがいいかも…
-        if (e.keyCode === 9) {
-          if (e.shiftKey === false) {
-            e.preventDefault();
-            var elem = e.target;
-            var val = elem.value;
-            var pos = elem.selectionStart;
-            elem.value = val.substr(0, pos) + '\t' + val.substr(pos, val.length);
-            elem.setSelectionRange(pos + 1, pos + 1);
-          } else {
-            e.preventDefault();
-            var elem = e.target;
-            var val = elem.value;
-            var pos = elem.selectionStart;
-            var firstPart = val.substr(0, pos).split("\n");
-            var cursorLine = firstPart.pop().replace(/^\t/, '');
-            firstPart = firstPart.join("\n") + "\n" + cursorLine;
-            elem.value = firstPart + val.substr(pos, val.length);
-            elem.setSelectionRange(pos - 1, pos - 1);
-          }
-        }
+      onDragStart: function(e) {
+        this.separateX = e.x;
+        this.previewWidth = parseInt($('#preview').css('width').replace('px', ''));
+        console.log('p = ' + this.previewWidth);
+      },
+      dragEnd: function(e) {
+        var diff = this.separateX - e.x + this.previewWidth;
+        $('#preview').css('width', diff + 'px');
+        $('#editor').css('width', e.x + 'px');
+        $('#separate').css('left', e.x);
+      },
+      dragging: function(e) {
+        var diff = this.separateX - e.x + this.previewWidth;
+        $('#preview').css('width', diff + 'px');
+        $('#editor').css('width', e.x + 'px');
+        $('#separate').css('left', e.x);
       }
     }
 });
@@ -61,12 +88,12 @@ if (client.isAuthenticated()) {
 
 
 function saveFile() {
-  console.log(jQuery('#content-input').val());
+  console.log(jQuery('#editor').val());
   var fileDialog = window.prompt("ファイル名を入力してください", "");
   if (fileDialog == '') {
     alert('ファイル名を入力してください');
   } else {
-    client.writeFile(fileDialog, jQuery('#content-input').val(), function (error) {
+    client.writeFile(fileDialog, jQuery('#editor').val(), function (error) {
       if (error) {
         console.log('Error : ' + error);
       } else {
