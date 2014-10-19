ae7c49d9cc5ef776e5914aa25fbcccc47de961d9
zaru@sakuraba <zarutofu@gmail.com>
2014-10-19 23:49:54 +0900

    herokuデプロイ用のフォーマットをコピーするように修正

--- Gruntfile.js
+++ Gruntfile.js
@@ -324,7 +324,10 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'package.json',
+            'Procfile',
+            'server.js'
           ]
         }, {
           src: 'node_modules/apache-server-configs/dist/.htaccess',
